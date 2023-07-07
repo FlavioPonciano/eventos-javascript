446a3464b5b7073d7b54c89805d0e6c66a78a476
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-07-07 16:49:09 -0300

    Upando arquivos
    Trabalhando com eventos JS

---
+++ eventos-mouse/index.js
@@ -0,0 +1,18 @@
+/*
+Existem vários tipo de eventos, os de mouse os principais são:
+onclick - quando o elemento é clicado
+ondblclick - quando o elemento é clicado duas vezes
+onmouseup - quando o clique sobre o elemento é liberado
+onmouseover - quando o cursor do mouse sobrepõem o elemento
+onmouseout - quando o cursor sai da area do elemento
+
+Nem todos os eventos são aceitos em todos os elementos html para saber se o evento é aceito é interessante consultar a documentação na w3schools na parte de DOM events
+*/
+
+function acao() {
+    alert('Evento Disparado')
+}
+
+function acao2() {
+    alert('Mouse fora do elemento')
+}
---
+++ eventos-teclado/index.js
@@ -0,0 +1,10 @@
+/*
+Eventos a partir do TECLADO:
+onkeydown - quando a tecla é apertada 
+onkeypress - quando a tecla é liberada (só quando a tecla representa um caractere)
+onkeyup - quando matém a tecla pressionada
+*/
+
+function acao1() {
+    alert('Evento disparado')
+}
