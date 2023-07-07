e5dee72b7efd22ff8b2f9dc19a96c613b763c0e3
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-07-07 17:40:36 -0300

    Upando arquivos
    Evento de Janela

---
+++ eventos-formularios/index.js
@@ -0,0 +1,6 @@
+/*
+Eventos em Formulários:
+onfocus - quando o elemento recebe o focu do cursor do mouse
+onblur - quando o elemento perde o foco
+onchange - quando o estado do elemento é alterado
+*/
